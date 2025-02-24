0e850a543c0cfd1e6661640a3de91ed3a4efdf87
87 <dev@87flowers.com>
2025-02-24 15:54:41 +0000

    add pdep

--- src/lb/util/pext.h
+++ src/lb/util/pext.h
@@ -9,7 +9,7 @@ namespace lb {
   constexpr auto pextSlow(u64 x, u64 m) -> u64 {
     u64 result = 0;
     for (u64 b = 1; m != 0; b += b) {
-      if (x & m & -m != 0)
+      if ((x & m & -m) != 0)
         result |= b;
       m &= m - 1;
     }
@@ -20,6 +20,22 @@ namespace lb {
     if (std::is_constant_evaluated())
       return pextSlow(x, m);
     return _pext_u64(x, m);
+  }
+
+  constexpr auto pdepSlow(u64 x, u64 m) {
+    u64 result = 0;
+    for (u64 b = 1; m != 0; b += b) {
+      if ((x & b) != 0)
+        result |= m & -m;
+      m &= m - 1;
+    }
+    return result;
+  }
+
+  constexpr auto pdep(u64 x, u64 m) -> u64 {
+    if (std::is_constant_evaluated())
+      return pdepSlow(x, m);
+    return _pdep_u64(x, m);
   }
 
   template <typename T> constexpr auto pinc(T x, T m) -> T { return static_cast<T>(x - m) & m; }
